4b0e7ecf93006795dbf217d4e6b58da33bb2b6c0
Onkar Magdum <onkarm533@gmail.com>
2021-09-09 20:40:51 +0530

    commit

--- client/src/utils/url.js
+++ client/src/utils/url.js
@@ -1,2 +1,2 @@
 // export const API = 'https://i-network-onkar.herokuapp.com'
-// export const BASE_URL = 'https://i-network-onkar.herokuapp.com'
+export const BASE_URL = 'https://i-network-onkar.herokuapp.com'
